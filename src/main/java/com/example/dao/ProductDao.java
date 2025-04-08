4ffaca1e87fe07889d6ab45619887967ab7718ed
justin <j7516761@gmail.com>
2025-04-08 14:17:35 +0800

    [Featrue][Product]  ProductDAO

---
+++ src/main/java/com/example/dao/ProductDao.java
@@ -0,0 +1,20 @@
+package com.example.dao;
+
+import java.util.List;
+
+import com.example.pojo.entity.Category;
+import com.example.pojo.entity.Product;
+
+public interface ProductDao {
+	Product findById(Long id);
+
+	List<Product> findAll();
+
+	List<Product> findByCategory(Category category);
+
+	void save(Product product);
+
+	void update(Product product);
+
+	void delete(Product product);
+}
