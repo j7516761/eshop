9655844a80ae340e0c0a41c738cd58257f3d7b51
justin <j7516761@gmail.com>
2025-04-08 14:17:35 +0800

    [Featrue][Product]  ProductDAO

---
+++ src/main/java/com/example/dao/impl/ProductDaoImpl.java
@@ -0,0 +1,62 @@
+package com.example.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.dao.ProductDao;
+import com.example.pojo.entity.Category;
+import com.example.pojo.entity.Product;
+
+@Repository
+public class ProductDaoImpl implements ProductDao {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public ProductDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    private Session getCurrentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return getCurrentSession().get(Product.class, id);
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return getCurrentSession()
+            .createQuery("FROM Product", Product.class)
+            .getResultList();
+    }
+
+    @Override
+    public List<Product> findByCategory(Category category) {
+        return getCurrentSession()
+            .createQuery("FROM Product p WHERE p.category = :category", Product.class)
+            .setParameter("category", category)
+            .getResultList();
+    }
+
+    @Override
+    public void save(Product product) {
+        getCurrentSession().persist(product);
+    }
+
+    @Override
+    public void update(Product product) {
+        getCurrentSession().merge(product);
+    }
+
+    @Override
+    public void delete(Product product) {
+        getCurrentSession().remove(product);
+    }
+}
